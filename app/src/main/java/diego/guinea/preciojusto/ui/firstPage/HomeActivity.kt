c7017dfedccb8721ac2c5f40cb7e0770fd6b33cb
cxb0360 <diegoguineasds@gmail.com>
2021-03-19 11:22:45 +0100

    HomePage commit-4
    HomePage terminado

--- app/src/main/java/diego/guinea/preciojusto/ui/firstPage/HomeActivity.kt
+++ app/src/main/java/diego/guinea/preciojusto/ui/firstPage/HomeActivity.kt
@@ -1,10 +1,12 @@
 package diego.guinea.preciojusto.ui.firstPage
 
+import android.animation.ValueAnimator
 import android.content.Intent
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.animation.AnimationUtils
+import android.view.animation.LinearInterpolator
 import android.widget.ImageView
 import android.widget.TextView
 import diego.guinea.preciojusto.R
@@ -33,14 +35,26 @@ class HomeActivity : AppCompatActivity() {
 //    }
 
     private fun prepareBackground() {
-
-//        val playText = findViewById<TextView>(R.id.play_text)
 //        //val fondo = findViewById<ExoPlayerView>(R.id.videoDeFondo)
         val playButton = findViewById<ImageView>(R.id.play_button)
         val settingsButton = findViewById<ImageView>(R.id.settings_button)
-//        val animationLeft = AnimationUtils.loadAnimation(this, R.anim.animacion_left)
-//        val animationBottom = AnimationUtils.loadAnimation(this, R.anim.animacion_bottom)
-//
+        val coin_one = findViewById<ImageView>(R.id.coinGif)
+        val coin_two = findViewById<ImageView>(R.id.coinGif2)
+        val cont = 0
+
+        val valueAnimator = ValueAnimator.ofFloat(0f,360f)
+
+        valueAnimator.addUpdateListener {
+            val value = it.animatedValue as Float
+            coin_one.rotation = value
+            coin_two.rotation = value
+        }
+
+        valueAnimator.interpolator = LinearInterpolator()
+        valueAnimator.duration = 3000
+        valueAnimator.repeatCount = ValueAnimator.INFINITE
+        valueAnimator.start()
+
         playButton.setOnClickListener{
             val intent = Intent(this, GamePage::class.java)
             startActivity(intent)
@@ -49,9 +63,7 @@ class HomeActivity : AppCompatActivity() {
             val intent = Intent(this, Settings::class.java)
             startActivity(intent)
         }
-//        settingsButton.animation = animationLeft
-//        playText.animation = animationBottom
-//        playButton.animation = animationBottom
+
 
         //fondo.prepare(Uri.parse(VideoFondo))
     }
