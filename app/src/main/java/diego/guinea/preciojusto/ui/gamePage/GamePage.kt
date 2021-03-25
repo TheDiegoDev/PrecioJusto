86f2157d41b470291d005a4ef0704b5bc4669818
cxb0360 <diegoguineasds@gmail.com>
2021-03-25 19:13:13 +0100

    GamePage commit-4

--- app/src/main/java/diego/guinea/preciojusto/ui/gamePage/GamePage.kt
+++ app/src/main/java/diego/guinea/preciojusto/ui/gamePage/GamePage.kt
@@ -3,9 +3,11 @@ package diego.guinea.preciojusto.ui.gamePage
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
+import com.bumptech.glide.Glide
 import diego.guinea.preciojusto.R
 import diego.guinea.preciojusto.data.modelo.ObjectsPJ
 import diego.guinea.preciojusto.data.modelo.ObjectsPrice
+import kotlinx.android.synthetic.main.activity_game.*
 import org.koin.android.ext.android.inject
 
 
@@ -13,6 +15,7 @@ class GamePage : AppCompatActivity() {
 
     private val viewModel by inject<GamePageViewModel>()
     private val pjObject: ArrayList<ObjectsPJ> = arrayListOf()
+    lateinit var values: ObjectsPrice
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,11 +26,16 @@ class GamePage : AppCompatActivity() {
 
     private fun observer() {
         viewModel.valuesViewMLD.observe(this, Observer {
-         // getData(it.results)
+          getData(it)
         })
     }
 
-    private fun getData(it: List<ObjectsPJ>) {
-        //pjObject.addAll(it)
+    private fun getData(it: ObjectsPrice) {
+        values = it
+        pjObject.addAll(it.objetos)
+        Glide.with(imageObject.context)
+                .load(pjObject[0].foto)
+                .into(imageObject)
+        textDescripcion.text = pjObject[0].descripcion
     }
 }
