c36f0df352e74efb836375f47773c952b23c17dc
TheDiegoDev <diegoguineasds@gmail.com>
2021-05-05 16:46:51 +0200

    chose level commit-2

---
+++ app/src/main/java/diego/guinea/preciojusto/data/modelo/AlphaChar.kt
@@ -0,0 +1,12 @@
+package diego.guinea.preciojusto.data.modelo
+
+class AlphaChar {
+
+    var iconChar: Int ? = 0
+    var alphaChar: String ? = null
+
+    constructor(iconChar: Int?, alphaChar: String?) {
+        this.iconChar = iconChar
+        this.alphaChar = alphaChar
+    }
+}
