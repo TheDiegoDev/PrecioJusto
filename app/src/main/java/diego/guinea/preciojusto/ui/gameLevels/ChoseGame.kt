df58838376bc229d8edd3f1c27cbf66b17ac2351
TheDiegoDev <diegoguineasds@gmail.com>
2021-05-05 13:10:54 +0200

    ChoseGame commit-1

---
+++ app/src/main/java/diego/guinea/preciojusto/ui/gameLevels/ChoseGame.kt
@@ -0,0 +1,14 @@
+package diego.guinea.preciojusto.ui.gameLevels
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import diego.guinea.preciojusto.R
+
+class ChoseGame : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_chose_levels)
+    }
+
+
+}
