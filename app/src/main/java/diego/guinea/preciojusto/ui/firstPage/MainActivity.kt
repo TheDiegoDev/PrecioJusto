05edf20b3f761d6595731015000b19ee1e38ca99
TheDiegoDev <diegoguineasds@gmail.com>
2021-05-10 17:21:28 +0200

    Settings Sound intentando poner sonido en toda la app

--- app/src/main/java/diego/guinea/preciojusto/ui/firstPage/MainActivity.kt
+++ app/src/main/java/diego/guinea/preciojusto/ui/firstPage/MainActivity.kt
@@ -1,6 +1,7 @@
 package diego.guinea.preciojusto.ui.firstPage
 
 import android.content.Intent
+import android.media.MediaPlayer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
@@ -9,12 +10,25 @@ import android.widget.ImageView
 import android.widget.TextView
 import diego.guinea.preciojusto.R
 
+
 class MainActivity : AppCompatActivity() {
+
+   // private lateinit var mp: SoundApp
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSplash()
+        BackgroundSound()
+    }
 
+    private fun BackgroundSound() {
+//        mp = SoundApp()
+//        mp.player.start()
+//        mp = MediaPlayer.create(this, R.raw.preciojusto)
+//        mp.isLooping = true
+//        mp.setVolume(100f, 100f)
+//        mp.start()
     }
 
     private fun setSplash() {
